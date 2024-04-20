c59f0757dda564f15c0b7027fff22ff0cd33366c
toubal zine-eddine <toubalzineddine77@gmail.com>
2024-04-20 09:07:35 +0200

    gestion redirection page error

--- src/main/java/fr/univrouen/cv24/controllers/IndexController.java
+++ src/main/java/fr/univrouen/cv24/controllers/IndexController.java
@@ -1,12 +1,15 @@
 package fr.univrouen.cv24.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @GetMapping(value = "/")
+    @GetMapping(value =  {"/", "/index"})
     public String index() {
         return "index";
     }
@@ -19,5 +22,12 @@ public class IndexController {
     @GetMapping(value = "/error")
     public String error() {
         return "error";
+    }
+    
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex) {
+       ModelAndView modelAndView = new ModelAndView();
+      modelAndView.setViewName("error");
+        return modelAndView;
     }
 }
