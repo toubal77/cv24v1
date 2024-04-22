54960c8267a742d3742b9b2c55e778e8cb8e6c53
toubal zine-eddine <toubalzineddine77@gmail.com>
2024-04-22 20:01:30 +0200

    check if cv existe

--- src/main/java/fr/univrouen/cv24/services/CV2Service.java
+++ src/main/java/fr/univrouen/cv24/services/CV2Service.java
@@ -1,5 +1,7 @@
 package fr.univrouen.cv24.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,11 @@ import fr.univrouen.cv24.model.CV24;
 import fr.univrouen.cv24.model.CV24type;
 import fr.univrouen.cv24.model.TestCV;
 import fr.univrouen.cv24.repositorie.CVRepositorie;
+import jakarta.persistence.spi.TransformerException;
 
 @Service
 public class CV2Service {
-    
-     
+
       
     
         @Autowired
@@ -22,26 +24,38 @@ public class CV2Service {
        
     
     
+        public Boolean searchCV24(TestCV cv24) {
+            List<CV24type> cvList = stbRepository.findAll();
 
+            for (CV24type cv : cvList) {
+                if (cv.getIdentite().getGenre().equals(cv24.getIdentite().getGenre()) &&
+                    cv.getIdentite().getNom().equals(cv24.getIdentite().getNom()) &&
+                    cv.getIdentite().getPrenom().equals(cv24.getIdentite().getPrenom()) &&
+                    cv.getIdentite().getTel().equals(cv24.getIdentite().getTel())) {
+                    return true;
+           
+                }
+            }
+            
+            return false;
+        }
+        
     
-        public String insert(TestCV stb) {
-            if (stb != null)  {
-            //    Response response = validator.validateStb(mapper.marchall(stb));
-            //    if (response.getStatus() == Response.Type.OK) {
-                    // if (stbRepository.findByTitleAndVersion(stb.getTitle(), stb.getVersion()) != null) {
-                    //     return mapper.marchall(new Response(Response.Type.ERROR, "Flux déjà existant !"));
-                    // }
-                    CV24type stbEntity = stbRepository.save(CV24Mapper.INSTANCE.toEntity(stb));
+        public String insert(TestCV cv24) {
+        
+        
+                     if (searchCV24(cv24)) {
+                        return  "<message>" + 
+                    							
+                        "<status>"+ HttpStatus.CONFLICT +"</status>" + 
+                    "</message>";
+                     }
+                    CV24type stbEntity = stbRepository.save(CV24Mapper.INSTANCE.toEntity(cv24));
                     return  "<message>" + 
                     								
                      									"<status>"+ HttpStatus.OK +"</status>" + 
                  									"</message>";
-                } else {
-                    return  "<message>" + 
-                    								
-                    "<status>"+ HttpStatus.CONFLICT +"</status>" + 
-                "</message>";
-                }
+    
            // } else {
            //     return mapper.marchall(new Response(Response.Type.ERROR));
            // }
